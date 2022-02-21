671d46e462c06f1af94e4d51f7fc489f2180d17c
seojungmin <neraize@gmail.com>
2022-02-21 09:55:25 +0900

    함수의 결과를 바로 지정 override fun getCount() =3

--- app/src/main/java/com/nepplus/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/nepplus/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.nepplus.viewpager_fragment_20220218.fragments.*
 
 class MainViewPagerAdapter(fm:FragmentManager):FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
+/*    override fun getCount(): Int {
         return 3
-    }
+    }*/
+
+    override fun getCount() =3
 
     override fun getItem(position: Int): Fragment {
 
